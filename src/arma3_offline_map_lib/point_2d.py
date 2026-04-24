9c9a9aca7b4bd97c802980902787518e70dba890
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2026-04-24 13:03:38 +0100

    feat: Point2D (use attrs)

---
+++ src/arma3_offline_map_lib/point_2d.py
@@ -0,0 +1,16 @@
+"""Module containing Point2D class."""
+
+from attrs import define, field, validators
+
+
+@define(frozen=True)
+class Point2D:
+    """Very simple 2D point class.
+
+    Hashable, unlike geojson library
+    Provides x and y keyword construction and properties,
+    unlike geojson or pygeojson library
+    """
+
+    x: float = field(validator=validators.instance_of(float))
+    y: float = field(validator=validators.instance_of(float))
