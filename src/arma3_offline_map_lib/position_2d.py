bec7d0d1ded1d3b7a6c4e080702cc10aab8cb233
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2026-06-24 18:28:56 +0100

    feat: add Position2D constructors

--- src/arma3_offline_map_lib/position_2d.py
+++ src/arma3_offline_map_lib/position_2d.py
@@ -1,6 +1,15 @@
 """Module containing Position2D class."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Self
+
 from attrs import define, field, validators
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from . import geojson
 
 
 @define(frozen=True)
@@ -20,3 +29,16 @@ class Position2D:
             validators.instance_of(int), validators.instance_of(float)
         )
     )
+
+    @classmethod
+    def from_a3_position(cls, seq: Sequence[float]) -> Self:
+        """Construct `Position2D` from Arma 3 position, which has xy and xzy forms.
+
+        Use when e.g. parsing `mission.sqm`.
+        """
+        return cls(x=seq[0], y=seq[-1])
+
+    @classmethod
+    def from_geojson_position(cls, position: geojson.Position) -> Self:
+        """Construct `Position2D` from GeoJSON `Point`."""
+        return cls(x=position[0], y=position[1])
