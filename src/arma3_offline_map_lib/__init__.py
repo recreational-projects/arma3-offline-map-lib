c1eebc8c65fb4cb03e6690bb363091e71da22bf4
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2026-09-19 11:48:31 +0100

    feat: `mission.mission_sqm` -> `mission_sqm`

--- src/arma3_offline_map_lib/__init__.py
+++ src/arma3_offline_map_lib/__init__.py
@@ -3,7 +3,7 @@
 Submodules:
 * `arma3_offline_map_lib.grad_meh` provides an interface for Arma 3 map data
    exported with [`gruppe-adler/grad_meh`](https://github.com/gruppe-adler/grad_meh).
-* `arma3_offline_map_lib.mission`
+* `arma3_offline_map_lib.mission_sqm`
    provides a partial interface for an Arma 3 `mission.sqm` file.
 * `arma3_offline_map_lib.position_2d` provides a `Position2D` class.
 """
